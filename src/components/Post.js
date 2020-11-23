425930078bd2595258e04db3bae105e0234829da
hurleygr <55817822+hurleygr@users.noreply.github.com>
2020-11-23 17:52:14 -0500

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -12,7 +12,7 @@ class Post extends React.Component {
 render() {
     return (
         <div style = {{border: '2px solid', margin: '8px', marginRight: '30%'}}>
-            <Link style = {{color: "black", textDecoration: "none"}} to={{pathname:'/specificpost', state: {title:this.props.title, content:this.props.content, group:this.props.group, id:this.props.id}}} >
+            <Link style = {{color: "black", textDecoration: "none"}} to={{pathname:'/specificpost', state: {title:this.props.title, content:this.props.content, group_name:this.props.group_name, post_id:this.props.post_id}}} >
 
             <h1>{this.props.title}</h1>
 
@@ -23,7 +23,7 @@ render() {
             <p>{this.props.content}</p>
             <p>{this.props.create_date}</p>
             </Link>
-            <Buttons title={this.props.title} content={this.props.content} group={this.props.group} id={this.props.id} editfunc={this.props.editfunc} deletefunc={this.props.deletefunc}/>
+            <Buttons title={this.props.title} content={this.props.content} group={this.props.group} post_id={this.props.post_id} idx={this.props.idx} editfunc={this.props.editfunc} deletefunc={this.props.deletefunc}/>
         </div> 
     );
 } }
