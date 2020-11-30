eb18f8114364f5615f37fbe67c57697f6592b245
Griffin Hurley <hurleygr@flip2.engr.oregonstate.edu>
2020-11-30 11:38:29 -0800

    Basic navigation in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login';
-
+import { Link } from "react-router-dom";
 class Header extends React.Component {
     constructor(props) {
         super(props);
@@ -19,8 +19,23 @@ render() {
             <header style = {{position: 'fixed', width: "100%", top: "0px", marginBottom: "50px", backgroundColor: "black", color:"white"}}>
             <h1 style = {{display: "inline-block", marginLeft: "10px"}}> Not A Reddit Clone </h1>
             <h4 style = {{float: 'right', display: "inline-block", marginRight: "10px"}}> {"Logged in as: " + this.state.user} <Login func={this.loginFunc.bind(this)}/> </h4>
-            
-
+          <div>
+	   <Link to="/home">
+		<button type="button">
+		  Home
+		</button>
+	  </Link> 
+	  <Link to="/group">
+                <button type="button">
+                  Groups
+                </button>
+          </Link>
+	  <Link to="/creategroup">
+                <button type="button">
+                  Create Group
+                </button>
+          </Link>
+	  </div>
             </header>
             
 );
