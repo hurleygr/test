e14f46e5782ccce4f5daadc17c878de698b72794
hurleygr <55817822+hurleygr@users.noreply.github.com>
2020-11-30 18:52:40 -0500

    Update Login.js
    Separating login and registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {showLogin: false, showRegister: false, login: {username: null, password: null}, registration: {email: null, username: null, password: null}};
+        this.state = {showLogin: false, showLogOut: false, showRegister: false, login: {username: null, password: null}, registration: {email: null, username: null, password: null}};
 
     }
    
@@ -16,6 +16,15 @@ class Login extends React.Component {
         this.setState({registration: {...this.state.registration, password: e.target.value}})
     };
 
+    loginusername_change (e) {
+        
+        this.setState({login: {...this.state.login, username: e.target.value}})
+    };
+
+    loginpassword_change (e) {
+        this.setState({login: {...this.state.login, password: e.target.value}})
+    };
+	
     email_change (e) {
         this.setState({registration: {...this.state.registration, email: e.target.value}})
     };
@@ -24,12 +33,17 @@ class Login extends React.Component {
         this.setState({showRegister: !(this.state.showRegister)})
     };
     
+    show_logIn() {
+	 this.setState({showLogIn: !(this.state.showLogIn)})
+    };
+	
     close_register() {
         this.setState({showRegister: false})
     };
     log_out() {
 	localStorage.clear()
         this.props.func("Anonymous");
+	this.setState({showLogOut: false})
 	};
     registerFunction () {
         const clear_registration = {email: null, username: null, password: null};
@@ -50,18 +64,27 @@ class Login extends React.Component {
           .then(response => console.log(response))
 	      
           .catch(err => console.log(err))
-        localStorage.setItem("user", this.state.registration.username)
         this.props.func(this.state.registration.username)
 
 	this.setState({registration: clear_registration})
        
         this.close_register();
     };
-
+    isValidUser(username, password) {
+	  fetch('http://flip2.engr.oregonstate.edu:1135/login?username=' + username + 'password=' + password , {
+            
+          .then(response => response.json)
+	  .catch(err => console.log(err))
+	      
     logInFunction (e) {
         e.preventDefault();
-        this.props.func(this.state.login.username);
-        this.close_login() ;
+	const u = await isValidUser(this.state.login.username, this.state.login.password)
+	if (u.length) {
+		localStorage.setItem("user", this.state.login.username)
+        	this.props.func(this.state.login.username);
+  	else{
+		  alert("Invalid username or password")
+	this.setState({showLogOut: true})
     }
 
     
@@ -74,7 +97,10 @@ render() {
             
 
             <button onClick={this.show_register.bind(this)}>
-                Register/Log In
+                Register
+            </button>
+	    <button onClick={this.show_logIn.bind(this)}>
+                Log In
             </button>
 	    <button onClick={this.log_out.bind(this)}>
 		Log Out
@@ -97,7 +123,22 @@ render() {
             <input type="button" onClick={this.registerFunction.bind(this)} value="Submit"  />
         </form>
         : null}
-
+	
+	{this.state.showLogin ?
+        <form onSubmit = {this.logInFunction.bind(this)}>
+            <h2>Join:</h2>
+            
+            <label>
+                Username:
+                <input type="text" name="username" onChange = {this.loginusername_change.bind(this)} />
+            </label>
+            <label>
+                Password:
+                <input type="text" name="password" onChange = {this.loginpassword_change.bind(this)} />
+            </label>
+            <input type="button" onClick={this.logInFunction.bind(this)} value="Submit"  />
+        </form>
+        : null}
         
         </div>
     )
